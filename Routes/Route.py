505b5d0a7325cdecf84747d7a8c98a3e020618bb
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-13 08:12:14 +0100

    additions to the skeleton classs Route

--- Routes/Route.py
+++ Routes/Route.py
@@ -1,3 +1,15 @@
 class Route:
-    def __init__(self, starting_station, destination_station, arrival_time, departure_time, distance):
+    def __init__(self, starting_station: str, destination_station: str, arrival_time, departure_time, distance):
+        pass
+
+    def book_route(self):
+        pass
+
+    def is_booked(self) -> bool:
+        pass
+
+    def undo_book_route(self):
+        pass
+
+    def set_arrive_time_departure_time(self, arrival_time, departure_time):
         pass

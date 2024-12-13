042334a850010acee935a591c575319162b0664f
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-13 08:54:59 +0100

    Deciding to use networtx to help represensting routes

--- Routes/Route.py
+++ Routes/Route.py
@@ -1,4 +1,11 @@
+import networkx as nx
+
+
 class Route:
+    """Routes will be in nx graph 
+    each node of graph is station : data in node (starting_station, destination_station, departure_time, arrival_time)
+    each edge is road : data is (distance, data_booked)
+    """
     def __init__(self, starting_station: str, destination_station: str, arrival_time, departure_time, distance):
         pass
 
@@ -13,3 +20,5 @@ class Route:
 
     def set_arrive_time_departure_time(self, arrival_time, departure_time):
         pass
+
+

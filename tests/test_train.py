30b5af6a7d12b53595afa5b5e1b5ac3be36560b6
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-23 08:58:27 +0100

    Implemented basic test for carriage class

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,4 +1,9 @@
 from train.Seats import Seat
+from train.carriage import Cariage
+from Routes.Routes import Routes
+from Routes.Route import Route
+import pytest
+from datetime import datetime
 
 
 def test_create_seat():
@@ -33,3 +38,49 @@ def test_check_requriments_id():
     assert seat.check_requirments(r_data) is False
     r_data = {'compartments': True, 'id': 1, 'window_middle_corridor': 2}
     assert seat.check_requirments(r_data) is True
+
+
+def create_simple_CarriageRoutes():
+    data1 = datetime(2024, 12, 29, 20, 30)
+    data2 = datetime(2024, 12, 29, 17, 30)
+    route1 = Route("Warszawa Centralna", "Kraków", data1, data2, 300)
+    data11 = datetime(2024, 12, 29, 21, 30)
+    data22 = datetime(2024, 12, 29, 20, 45)
+    route2 = Route("Kraków", "Wrocław", data11, data22, 350)
+    carriage_routes = Routes(1, [route1, route2])
+    return carriage_routes
+
+
+def test_create_carriage():
+    seats = {Seat(1, True, 2),
+             Seat(2, True, 1),
+             Seat(3, True, 0)}
+
+    carriage_routes = create_simple_CarriageRoutes()
+    carriage = Cariage(1, [carriage_routes], seats)
+
+    assert carriage.id == 1
+    assert carriage.seats == seats
+
+
+def test_create_carriage_list_avalible_seats():
+    seats = {Seat(1, True, 2),
+             Seat(2, True, 1),
+             Seat(3, True, 0)}
+
+    carriage_routes = create_simple_CarriageRoutes()
+    carriage = Cariage(1, [carriage_routes], seats)
+    ans = carriage.list_all_availabe_seats('Warszawa Centralna', 'Wrocław', 1)
+    assert ans == ({1, 2, 3}, set())
+    assert carriage.seats == seats
+
+
+def test_create_carriage_list_avalible_seat_wrong_id_route():
+    seats = {Seat(1, True, 2),
+             Seat(2, True, 1),
+             Seat(3, True, 0)}
+
+    carriage_routes = create_simple_CarriageRoutes()
+    carriage = Cariage(1, [carriage_routes], seats)
+    with pytest.raises(ValueError):
+        carriage.list_all_availabe_seats('Warszawa Centralna', 'Wrocław', 2)

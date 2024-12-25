2769a8f0ec11f0f2a73c087eca7439b3ddbe0118
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-25 14:57:10 +0100

    Implemented user class with files

--- user/user.py
+++ user/user.py
@@ -1,2 +1,42 @@
+import json
+
+
 class User:
-    pass
+    def __init__(self, id, name: str, surname: str, password: str, login: str, tickets=None):
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self._password = password
+        self.login = login
+        self.tickets = tickets if tickets is not None else []
+
+    def __str__(self):
+        return f"{self.name}  {self.surname}"
+
+    def json_repr(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'surname': self.surname,
+            'password': self._password,
+            'login': self.login,
+            'tickets': [ticket.json_repre() for ticket in self.tickets]
+        }
+
+
+def write_user_file(user: User):
+    data = user.json_repr()
+    s1 = json.dumps(data, indent=4)
+    uniq_id = user.id
+    file_path = "/home/krzysztof-rutkowski/pipr1/project1/data/Users/" + f"{uniq_id}.json" # relative path add
+    with open(file_path, 'w') as file_handle:
+        file_handle.write(s1)
+
+
+def read_user(id: str):
+    file_path = "/home/krzysztof-rutkowski/pipr1/project1/data/Users/" + f"{id}.json" # relative path add
+    with open(file_path, 'r') as file_handle:
+        data = json.load(file_handle)
+
+    return User(data['id'], data['name'], data['surname'], data['password'], data['login'], data['tickets'])
+

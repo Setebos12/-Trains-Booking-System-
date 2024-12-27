fe7bb8c37860baa9dbc6b07457938ed2d763c54e
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-27 10:29:06 +0100

    Monitor user last choices

---
+++ System/MonitorUser.py
@@ -0,0 +1,19 @@
+class MonitorUserSystem():
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.deparute = None
+        self.arrival = None
+        self.train_id = None
+        self.route_id = None
+        self.carriage_id = None
+        self.seat_id = None
+
+    def check_if_all_not_none(self):
+        return all([
+            self.deparute is not None,
+            self.arrival is not None,
+            self.train_id is not None,
+            self.route_id is not None,
+            self.carriage_id is not None,
+            self.seat_id is not None
+        ])

d56dad7dbf65f2e30b50f295157bdbfa12fdc617
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-27 09:43:06 +0100

    Test user ticket file_handle

---
+++ tests/test_user.py
@@ -0,0 +1,30 @@
+from user.ticket import Ticket
+from user.user import User, write_user_file, read_user
+
+
+def test_create_ticket():
+    ticket = Ticket("Warsaw", "Vien", 1, 1, 1, 1)
+
+    assert ticket.carriage_id == 1
+    assert ticket.route_id == 1
+    assert ticket.train_id == 1
+    assert ticket.end_station == "Vien"
+    assert ticket.start_station == 'Warsaw'
+
+
+def test_create_user():
+    user = User(1)
+    assert user.id == 1
+
+
+def test_write_read_user_file():
+    user = User(1)
+    ticket = Ticket("Warsaw", "Vien", 1, 1, 1, 1)
+    user.add_ticket(ticket)
+
+    write_user_file(user)
+    user1 = read_user(1)
+
+    assert user1.id == 1
+    assert user1.tickets[0].carriage_id == 1
+    assert user1.tickets[0].start_station == 'Warsaw'

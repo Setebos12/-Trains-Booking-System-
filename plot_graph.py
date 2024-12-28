e8826ba9033e02186c90e0bb60763b2889a9659b
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-28 14:38:35 +0100

    impleneted plotting graph

---
+++ plot_graph.py
@@ -0,0 +1,21 @@
+import networkx as nx
+from matplotlib import pyplot as plt
+
+
+def draw_graph(routes):
+    G = routes
+    stations = routes.nodes
+    station_positions = get_stations_gps(stations)
+
+    plt.figure(figsize=(10, 8))
+    nx.draw(G, pos=station_positions, with_labels=True, node_color="skyblue", font_weight="bold", node_size=200, font_size=6)
+    plt.show()
+
+
+def get_stations_gps(stations):
+    station_positions = {}
+
+    for station in stations:
+        name, address, gps = get_station_data("https://portalpasazera.pl/KatalogStacji/Index?stacja=" + str(station))
+        station_positions[station] = (float(gps[1].replace(',', '.')), float(gps[0].replace(',', '.')))
+    return station_positions

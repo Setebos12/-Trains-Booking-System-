94aa2b97fd9e085fdbcbd67ab42c4d0f0be8ceb4
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-28 12:38:53 +0100

    Adjust to Flake

--- Routes/Route.py
+++ Routes/Route.py
@@ -6,12 +6,12 @@ class NotDateTimeDatError(Exception):
 
 
 class Route:
-    def __init__(self, starting_station: str, destination_station: str, arrival_time, departure_time, distance):
+    def __init__(self, starting_station: str, destination_station: str,
+                 arrival_time, departure_time, distance):
         self.starting_station = starting_station
         self.destination_station = destination_station
         self.set_arrive_time_departure_time(arrival_time, departure_time)
-        self.set_disctance(distance)
-
+        self.set_distance(distance)
         self.booked_data = None
 
     def arrival_time(self):
@@ -20,20 +20,22 @@ class Route:
     def departure_time(self):
         return self._departure_time
 
-    def set_disctance(self, distance):
+    def set_distance(self, distance):
         if distance < 0:
-            return ValueError
+            raise ValueError("Distance cannot be negative.")
         self._distance = distance
 
     def distance(self):
         return self._distance
 
-    def set_arrive_time_departure_time(self, arrival_time, departure_time):
-        if type(arrival_time) is not datetime or type(departure_time) is not datetime:
-            raise NotDateTimeDatError(("Both arrival_time and departure_time must be datetime objects."))
+    def set_arrive_time_departure_time(self, arrival_time: datetime,
+                                       departure_time: datetime):
+        if not isinstance(arrival_time, datetime) or not isinstance(
+                departure_time, datetime):
+            raise NotDateTimeDatError(
+                "Arrival_time and departure_time must be datetime objects.")
         if arrival_time < departure_time:
-            raise ValueError("Arrival time cannot be earlier than departure time.")
+            raise ValueError(
+                "Arrival time cannot be earlier than departure time.")
         self._arrival_time = arrival_time
         self._departure_time = departure_time
-
-

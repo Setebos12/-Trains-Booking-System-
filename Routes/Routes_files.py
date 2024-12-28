14f90fa7fb424ac14359c03fe0c17b8c9e8aceaa
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-28 12:17:49 +0100

    Simplifi Route Files

--- Routes/Routes_files.py
+++ Routes/Routes_files.py
@@ -1,39 +1,32 @@
 import json
 from Routes.Routes import Routes
-from datetime import datetime
 from networkx.readwrite.json_graph import node_link_graph
-
-def serialize_datetime(obj):
-    if isinstance(obj, datetime):
-        return obj.isoformat()
-    raise TypeError("Type not serializable")
-
-
-def deserialize_datetime(obj):
-    for key, value in obj.items():
-        if isinstance(value, str):
-            try:
-                obj[key] = datetime.fromisoformat(value)
-            except ValueError:
-                pass
-    return obj
-
+from file_handle import serialize_datetime, deserialize_datetime, write_data, read_data
 
 
 def write_Route(route: Routes):
-    data = route.json_repr()
-    s1 = json.dumps(data, default=serialize_datetime, indent=4)
-    uniq_id = str(route.routes_id)
-    file_path = "/home/krzysztof-rutkowski/pipr1/project1/data/Routes_files/" + f"{uniq_id}.json" # relative path add
-    with open(file_path, 'w') as file_handle:
-        file_handle.write(s1)
+    # data = route.json_repr()
+    # s1 = json.dumps(data, default=serialize_datetime, indent=4)
+    # uniq_id = str(route.id)
+    # file_path = (
+    #     "/home/krzysztof-rutkowski/pipr1/project1/data/Routes_files/"
+    #     f"{uniq_id}.json"
+    # )
+    # with open(file_path, 'w') as file_handle:
+    #     file_handle.write(s1)
+    write_data(route, base_path="data/Routes_files")
 
 
 def read_Route(uniq_id):
-    file_path = "/home/krzysztof-rutkowski/pipr1/project1/data/Routes_files/" + f"{uniq_id}.json" # relative path add
-    with open(file_path, 'r') as file_handle:
-        file_data = file_handle.read()
+    # file_path = (
+    #     "/home/krzysztof-rutkowski/pipr1/project1/data/Routes_files/"
+    #     f"{uniq_id}.json"
+    # )
+    # with open(file_path, 'r') as file_handle:
+    #     file_data = file_handle.read()
 
-    data = json.loads(file_data, object_hook=deserialize_datetime)
-    routes = Routes(data['id'], node_link_graph(data['graph'], edges="links"), False)
+    data = read_data(uniq_id, base_path="data/Routes_files")
+    routes = Routes(
+        data['id'], node_link_graph(data['graph'], edges="edges"), False
+    )
     return routes

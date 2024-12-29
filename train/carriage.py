1f796d59421a5a37ad369302a0672eee4889e5c2
Krzysztof Rutkowski <01192010@pw.edu.pl>
2024-12-29 12:57:58 +0100

    simplify carriage files

--- train/carriage.py
+++ train/carriage.py
@@ -3,36 +3,48 @@ from train.Seats import Seat
 
 
 class Cariage:
-    def __init__(self, id: int, routes: list[Routes], seats: list[Seat], carriage_look=None, initiation=True) -> None:
+    def __init__(self, id: int, routes: list[Routes], seats: list[Seat],
+                 carriage_look=None, initiation=True) -> None:
         self.id = id
         self.seats = {seat.data['id']: seat for seat in seats}
         self.seats_id = [seat.data['id'] for seat in self.seats.values()]
 
-        if initiation is True:
-            self.routes = {route.id: CarriageRoutes(route.id, route, self.seats_id) for route in routes}
-            if carriage_look is not None:
-                self.carriage_look = self.assing_seats(carriage_look)
-            else:
-                self.carriage_look = None
+        if initiation:
+            self._initialize(routes, carriage_look)
         else:
             self.routes = routes
             self.carriage_look = carriage_look
 
         self.current_route_id = 0
 
-    def book_seat_for_route(self, starting_station, destination_station, seat_id, route_id, data, time=None):
+    def _initialize(self, routes: list[Routes], carriage_look=None):
+        self.routes = {route.id: CarriageRoutes(route.id,
+                                                route,
+                                                self.seats_id)
+                       for route in routes}
+        if carriage_look is not None:
+            self.carriage_look = self.assing_seats(carriage_look)
+        else:
+            self.carriage_look = None
+
+    def book_seat_for_route(self, starting_station, destination_station,
+                            seat_id, route_id, data):
         if route_id not in self.routes:
             raise ValueError
 
-        self.routes[route_id].booked_seats(starting_station, destination_station, seat_id, data)
+        self.routes[route_id].booked_seats(starting_station,
+                                           destination_station,
+                                           seat_id, data)
 
-    def list_all_availabe_seats(self, starting_station, destination_station, route_id, r_data={}):
+    def list_all_availabe_seats(self, starting_station, destination_station,
+                                route_id, r_data={}):
         if route_id not in self.routes:
             raise ValueError
-        free_seats, booked_seats = self.routes[route_id].list_booked_and_an_empy_seats(starting_station, destination_station)
+        free_s, book_s = self.routes[route_id].list_booked_and_empty_seats(
+            starting_station, destination_station)
         seats_id = self.filter_seats(r_data)
-        free_seats = set(free_seats) & set(seats_id)
-        return free_seats, booked_seats
+        free_s = set(free_s) & set(seats_id)
+        return free_s, book_s
 
     def filter_seats(self, r_data):
         seats_id = set()
@@ -44,12 +56,12 @@ class Cariage:
     def assing_seats(self, carriage_look):
         seats_id = self.seats_id
         seats_id.sort(key=int)
-        print(seats_id)
         index = 0
         for x_dim in range(len(carriage_look)):
             for y_dim in range(len(carriage_look[x_dim])):
                 if carriage_look[x_dim][y_dim] == 'S':
-                    carriage_look[x_dim][y_dim] = str(carriage_look[x_dim][y_dim]) + str(seats_id[index])
+                    carriage_look[x_dim][y_dim] = str(
+                        carriage_look[x_dim][y_dim]) + str(seats_id[index])
                     index += 1
                     if index > len(seats_id):
                         return
@@ -76,10 +88,11 @@ class Cariage:
     def add_routes(self, route: Routes):
         self.routes[route.id] = CarriageRoutes(route.id, route, self.seats_id)
 
-    def carriage_repr(self):
+    def json_repr(self):
         return {
             'id': self.id,
             'seats': [seat.seat_repre() for seat in self.seats.values()],
             'carriage_look': self.carriage_look,
-            'graph': {route.id: route.json_repr() for route in self.routes.values()}
+            'graph': {route.id: route.json_repr()
+                      for route in self.routes.values()}
         }
--- train/carriage_files.py
+++ train/carriage_files.py
@@ -6,21 +6,10 @@ from file_handle import read_data, write_data
 
 
 def write_to_file_carriage(carriage: Cariage):
-    # data = carriage.carriage_repr()
-    # s1 = json.dumps(data, default=serialize_datetime, indent=4)
-    # uniq_id = str(carriage.id)
-    # file_path = "/home/krzysztof-rutkowski/pipr1/project1/data/Carriages/" + f"{uniq_id}.json" # relative path add
-    # with open(file_path, 'w') as file_handle:
-    #     file_handle.write(s1)
     write_data(carriage, base_path="data/Carriages")
 
 
 def read_carriage(uniq_id):
-    # file_path = "/home/krzysztof-rutkowski/pipr1/project1/data/Carriages/" + f"{uniq_id}.json" # relative path add
-    # with open(file_path, 'r') as file_handle:
-    #     file_data = file_handle.read()
-
-    # data = json.loads(file_data, object_hook=deserialize_datetime)
     data = read_data(uniq_id, base_path="data/Carriages")
     return create_carriage_from_data(data)
 
